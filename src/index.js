f5c3afa7d8b2da90b4e9c0458bbf97057684b944
VladyslavLata <vladlatahtc@gmail.com>
2022-06-24 16:57:16 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-import MyNewsApi from "./logics";
-
+import MyNewsApi from './logics';
 
 const formEl = document.querySelector('.form');
-const btnPrimaryEl = document.querySelector('.btn-ptimary');
 const btnSeccondEl = document.querySelector('.btn-seccond');
+const listEl = document.querySelector('.list');
 formEl.addEventListener('submit', onFetchInfo);
 btnSeccondEl.addEventListener('click', onMoreFetchInfo);
 
 const myNewsApi = new MyNewsApi();
 
+btnSeccondEl.classList.add('is-hiden');
 
-
-function onFetchInfo() {
+function onFetchInfo(event) {
   event.preventDefault();
-  myNewsApi.searchQuery = event.currentTarget.elements.query.value;
-  myNewsApi.fetchInfo();
+
+  if (event.currentTarget.elements.query.value === '') {
+    return window.alert('Введите парамерт поиска');
+  } else if (myNewsApi.search === event.currentTarget.elements.query.value) {
+    return;
+  }
+  myNewsApi.pageReset();
+  myNewsApi.search = event.currentTarget.elements.query.value;
+  myNewsApi.fetchInfo().then(articles => {
+    clearList();
+    addMarkupData(articles);
+    btnSeccondEl.classList.remove('is-hiden');
+  });
 }
 
 function onMoreFetchInfo() {
+  myNewsApi.fetchInfo().then(addMarkupData);
+}
+
+function addMarkupData(articles) {
+  markup = articles.map(markupData).join('');
+  listEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearList() {
+  listEl.innerHTML = '';
+}
+
+function markupData({ title, description, urlToImage }) {
+  return `<li>
+  <img src="${urlToImage}" alt="" width="300px">
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </li>`;
 }
--- src/logics.js
+++ src/logics.js
@@ -1,12 +1,45 @@
-export default class MyNewsApi {
-  constructor () {
-    this.searchQuery = '';
-}
+const MY_NEWSAPI_KEY = 'a05ceaa7c0394871ad19df204a773bc7';
 
-  fetchInfo() {
-    const MY_NEWSAPI_KEY = "a05ceaa7c0394871ad19df204a773bc7";
-   return fetch(`https://newsapi.org/v2/everything?q=${this.searchQuery}&apiKey=${MY_NEWSAPI_KEY}&pageSize=5&page=1`).
-      then(r => r.json()).then(a => console.log(a));
+export default class MyNewsApi {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
   }
 
+  fetchInfo() {
+    const params = new URLSearchParams({
+      q: `${this.searchQuery}`,
+      apiKey: `${MY_NEWSAPI_KEY}`,
+      pageSize: 5,
+      page: `${this.page}`,
+    });
+    return fetch(`https://newsapi.org/v2/everything?${params}`)
+      .then(respons => {
+        if (!respons.ok) {
+          throw new Error(respons.status);
+        }
+        return respons.json();
+      })
+      .then(datas => {
+        this.pageIncrement();
+        return datas.articles;
+      })
+      .catch(error => console.log(error));
+  }
+
+  pageIncrement() {
+    this.page += 1;
+  }
+
+  pageReset() {
+    this.page = 1;
+  }
+
+  get search() {
+    return this.searchQuery;
+  }
+
+  set search(newSearch) {
+    this.searchQuery = newSearch;
+  }
 }
